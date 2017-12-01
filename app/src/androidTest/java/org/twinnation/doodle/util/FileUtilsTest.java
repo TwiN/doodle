69b2b4fce418a89d84081ccc62b49e82a56b0884
TwinProduction <christian-ct@hotmail.com>
2017-12-01 15:45:01 -0500

    Added a very useless

---
+++ app/src/androidTest/java/org/twinnation/doodle/util/FileUtilsTest.java
@@ -0,0 +1,18 @@
+package org.twinnation.doodle.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * I feel like this is the most useless test I've ever made, but I am forced to do this.
+ * Please forgive me.
+ */
+public class FileUtilsTest {
+
+    @Test
+    public void generateFilename() throws Exception {
+        assertTrue(FileUtils.generateFilename().startsWith("doodle_"));
+    }
+
+}
