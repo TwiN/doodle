bb32d614b95bdaa91803a810b8e8d25abd75d427
TwinProduction <christian-ct@hotmail.com>
2017-12-01 22:32:56 -0500

    Fixed instrumented tests

--- app/src/androidTest/java/org/twinnation/doodle/activity/DrawActivityTest.java
+++ app/src/androidTest/java/org/twinnation/doodle/activity/DrawActivityTest.java
@@ -1,6 +1,7 @@
-package org.twinnation.doodle;
+package org.twinnation.doodle.activity;
 
 
+import android.graphics.Color;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -9,13 +10,14 @@ import android.test.suitebuilder.annotation.LargeTest;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.twinnation.doodle.R;
 import org.twinnation.doodle.view.CanvasView;
-import org.twinnation.doodle.activity.DrawActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -24,17 +26,37 @@ import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class BrushSizeTest {
+public class DrawActivityTest {
 
     @Rule
     public ActivityTestRule<DrawActivity> mActivityTestRule = new ActivityTestRule<>(DrawActivity.class);
 
 
     @Test
+    public void colorPickerTest() {
+        CanvasView canvasView = mActivityTestRule.getActivity().getCanvasView();
+        assertThat("By default, the brush color should be black.",
+                canvasView.getCurrentColor(), is(Color.BLACK));
+
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.setColor), withContentDescription("Pick a color"), isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction appCompatImageView = onView(
+                allOf(withId(R.id.color_green), isDisplayed()));
+        appCompatImageView.perform(click());
+
+        assertThat("After picking the color green, the color of the brush should be green",
+                canvasView.getCurrentColor(), is(Color.GREEN));
+    }
+
+
+    @Test
     public void maxBrushSizeTest() {
         CanvasView canvasView = mActivityTestRule.getActivity().getCanvasView();
         ViewInteraction appCompatButton = onView(allOf(withId(R.id.plusSize), withText("+"),
-                        withParent(withId(R.id.bottomBar)), isDisplayed()));
+                withParent(withId(R.id.bottomBar)), isDisplayed()));
         int x = 15;
         while (x --> 0) {
             appCompatButton.perform(click());
---
+++ app/src/androidTest/java/org/twinnation/doodle/activity/SplashActivityTest.java
@@ -0,0 +1,49 @@
+package org.twinnation.doodle.activity;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.twinnation.doodle.R;
+
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SplashActivityTest {
+
+    @Rule
+    public ActivityTestRule<SplashActivity> mActivityTestRule = new ActivityTestRule<>(SplashActivity.class);
+
+
+    @Test
+    public void splashActivityTest() {
+        assertThat("The current window should be SplashActivity", mActivityTestRule.getActivity().hasWindowFocus(), is(true));
+
+        ViewInteraction appCompatTextView = onView(allOf(withId(R.id.drawBtn), withText("Draw"), isDisplayed()));
+        appCompatTextView.perform(click());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        assertThat("The current window should be DrawActivity", mActivityTestRule.getActivity().hasWindowFocus(), is(false));
+
+    }
+
+}
